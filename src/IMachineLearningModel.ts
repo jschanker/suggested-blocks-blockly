bc3ce541f95e4414bf35f347dc20ac3199ad5dbc
tpersaud2 <113070356+tpersaud2@users.noreply.github.com>
2024-10-23 12:27:28 -0400

    Update IMachineLearningModel.ts
    Array type using 'T[]' is forbidden for non-simple types. Use 'Array<T>' instead

--- src/IMachineLearningModel.ts
+++ src/IMachineLearningModel.ts
@@ -1,9 +1,9 @@
 import * as Blockly from 'blockly/core';
 
 interface MachineLearningModel {
-  getSuggestedBlocks(description: string): Blockly.Block[];
+  getSuggestedBlocks(description: string): Array<Blockly.Block>;
 
-  train(data: {description: string; blocks: Blockly.Block[]}[]): void;
+  train(data: {description: string; blocks: Array<Blockly.Block>}[]): void;
 }
 
 export default MachineLearningModel;
