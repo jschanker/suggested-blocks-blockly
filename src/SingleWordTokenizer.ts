a32aabf989f2c749118e5b961cb26c87324a458d
Kendra <kdominique1@lions.molloy.edu>
2024-12-07 22:26:14 -0500

    fixed singlewordtokenizer
    added tests for singlewordtokenizer and fixed the tokenize function

--- src/SingleWordTokenizer.ts
+++ src/SingleWordTokenizer.ts
@@ -1,9 +1,12 @@
 import Tokenizer from "./ITokenizer";
 
 class SingleWordTokenizer implements Tokenizer {
-    tokenize(a:string): string[] {
-        return a.toLocaleLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split("");
-        
+    tokenize(a: string): string[] {
+        return a
+            .toLowerCase()
+            .replace(/[^a-z0-9\s]/g, "")
+            .split(/\s+/)
+            .filter((token) => token.length > 0);
     }
 }
 
