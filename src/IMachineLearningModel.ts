49af048cd89a89b90a107c3706f28ee9c9ff9857
tpersaud2 <113070356+tpersaud2@users.noreply.github.com>
2024-12-15 21:56:17 -0500

    Update IMachineLearningModel.ts

--- src/IMachineLearningModel.ts
+++ src/IMachineLearningModel.ts
@@ -2,8 +2,8 @@ import * as Blockly from 'blockly/core';
 
 interface MachineLearningModel {
   getSuggestedBlocks(description: string): Array<Blockly.Block>;
-}
-  train(data: {description: string; blocks: Blockly.Block[]): void;
+
+  train(data: Array<{description: string; blocks: Blockly.Block[] }>): void;
 }
 
 export default MachineLearningModel;
