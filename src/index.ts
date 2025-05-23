6ff0dd12cbef6f9750e698e9079381294d3ea094
Kendra <kdominique1@lions.molloy.edu>
2025-05-23 14:08:43 -0400

    fixed lint and format errors after merge
    -removed word public from workspace and inputsource
    -ran prettier

--- src/index.ts
+++ src/index.ts
@@ -54,11 +54,10 @@ export class BlockSuggestor {
   /**
    * New workspace
    */
-  public workspace: Blockly.WorkspaceSvg | null = null;
+  workspace: Blockly.WorkspaceSvg | null = null;
   /**
    * Optional input description element or string
    */
-  public inputSource: HTMLInputElement | null = null;
   /**
    * Constructs a BlockSuggestor object.
    *
@@ -86,20 +85,20 @@ export class BlockSuggestor {
    * Generates block suggestions based on the current description.
    */
   getSuggestedBlocks = (): Blockly.utils.toolbox.BlockInfo[] => {
-    let description = ''; 
+    let description = '';
 
     if (typeof this.description === 'string') {
-       description = this.description;
-    } else if (this.inputSource instanceof HTMLInputElement) { 
+      description = this.description;
+    } else if (this.inputSource instanceof HTMLInputElement) {
       description = this.inputSource.value || '';
-     }
-     
-    const suggestedBlocks = this.model
-     ? this.model.getSuggestedBlocks(description)
-     : [];
+    }
 
-     const blockTypes = suggestedBlocks.map((block) => block.type);
-     return this.generateBlockData(blockTypes);
+    const suggestedBlocks = this.model
+      ? this.model.getSuggestedBlocks(description)
+      : [];
+
+    const blockTypes = suggestedBlocks.map((block) => block.type);
+    return this.generateBlockData(blockTypes);
   };
 
   /**
