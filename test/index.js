20798ea446f15e02cd208a8f6d9b1ccabf5408b4
Kendra <kdominique1@lions.molloy.edu>
2025-07-31 23:51:41 -0400

    modified to pass blockydiv
    -modified index.js to pass blocklydiv instead of workspace, ran prettier

--- test/index.js
+++ test/index.js
@@ -19,10 +19,9 @@ import * as SuggestedBlocks from '../src/index';
  * @returns {!Blockly.WorkspaceSvg} The created workspace.
  */
 function createWorkspace(blocklyDiv, options) {
-  // return SuggestedBlocks.init(blocklyDiv, options)
   const workspace = Blockly.inject(blocklyDiv, options);
   SuggestedBlocks.init(workspace);
-  return workspace;
+  return SuggestedBlocks.init(blocklyDiv, options);
 }
 
 const customTheme = Blockly.Theme.defineTheme('classic_with_suggestions', {
